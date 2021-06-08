676ffb1b725d80351199bc6fd27deb8bca72a76d
Roberto Soares neves <rsoares-@c1r2p1.42sp.org.br>
2021-06-08 12:16:55 -0300

    New fixes

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -5,13 +5,13 @@ char	*ft_strrchr(const char *s, int c)
 	int	d;
 
 	d = ft_strlen ((char *)s);
+	if (c == 0)
+		return ((char *)s + d);
 	while (d > 0)
 	{
-		if (s[d - 1] == c)
+		if (s[d - 1] == (c % 256))
 			return (((char *)&s[d - 1]));
 		d--;
 	}
-	if (c == 0)
-		return (((char *)&s[d - 1]));
 	return (NULL);
 }
