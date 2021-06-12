ce7ef2f6ff9361d85e6abfd882f1b52e2b77e3c5
Arnaldo Silva <rsnowsdev@gmail.com>
2021-06-12 16:00:42 -0300

    Testing final fixes

--- ft_substr.c
+++ ft_substr.c
@@ -4,19 +4,23 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
 	size_t	c;
+	size_t max;
 
 	if (!s)
 		return (NULL);
-	sub = malloc(sizeof(char) * len + 1);
+	max = (unsigned int)ft_strlen((char *)s);
+	if (max > len)
+		max = len;
+	sub = malloc(sizeof(char) * max + 1);
 	if (!sub)
 		return (NULL);
-	if ((unsigned int)ft_strlen((char *)s) < start)
+	if (max < start)
 	{
 		sub[0] = '\0';
 		return (sub);
 	}
 	c = 0;
-	while (c < len)
+	while (c < max)
 	{
 		sub[c] = s[start + c];
 		c++;
