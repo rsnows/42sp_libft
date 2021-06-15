faf3d731f4ff7c5eea76b6e149fac42bf81ee0e6
Roberto Soares neves <rsoares-@c1r2p3.42sp.org.br>
2021-06-15 16:15:06 -0300

    Complete bonus-less version

--- ft_substr.c
+++ ft_substr.c
@@ -8,6 +8,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	if (!s)
 		return (NULL);
+	if ((unsigned int)ft_strlen((char *)s) < len)
+		len = (unsigned int)ft_strlen((char *)s);
 	sub = malloc(sizeof(char) * (len + 1));
 	if (!sub)
 		return (NULL);
